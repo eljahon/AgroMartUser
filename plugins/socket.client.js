2713f98dac96056b7557bc065dab36c825bc581b
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-05-27 12:51:51 +0500

    aaa

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -2,9 +2,9 @@ import io from 'socket.io-client'
 let STRAPI_ENDPOINT
 
 if (process.env.NODE_ENV !== 'production') {
-  STRAPI_ENDPOINT = 'https://testsocket.agsat.uz'
+  STRAPI_ENDPOINT = 'https://socket.agsat.uz'
 } else {
-  STRAPI_ENDPOINT = 'https://testsocket.agsat.uz'
+  STRAPI_ENDPOINT = 'https://socket.agsat.uz'
 }
 
 export const socket = io(STRAPI_ENDPOINT)
